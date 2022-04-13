4693de7f4a9d268793660054d3c346b33628c70c
Alpha-gone <990131@naver.com>
2022-04-13 14:29:00 +0900

    Add Android Project

---
+++ app/src/main/java/com/obyte/alcohol/Bottombar.java
@@ -0,0 +1,14 @@
+package com.obyte.alcohol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bottombar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottombar);
+    }
+}
